1211489d6f2ba5df5fa36a89a49022d408c91c8f
joy <1519105541@qq.com>
2022-02-07 19:01:20 +0800

    1-4: 纯静态化

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,38 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default () => {
+  console.log('index render');
+  const [advs, setAdvs] = useState([]);
+  useEffect(() => {
+    console.log('加载广告数据');
+    setAdvs([
+      { link: 'https://www.baidu.com/', name: '百度' },
+      { link: 'https://www.sina.com.cn/', name: '新浪' },
+      { link: 'https://www.douyin.com/', name: '抖音' },
+      { link: 'https://duyiedu.com/', name: '渡一' },
+    ]);
+  }, []);
+
   return (
     <div>
       <Head>
         <title>首页</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <h1>Index 首页</h1>
+      <h1>Index 首页 {Math.random()}</h1>
+      <ul>
+        {advs.map((adv, index) => {
+          console.log(adv);
+          return (
+            <li key={index}>
+              <a href={adv.link} target="_blank">
+                {adv.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
